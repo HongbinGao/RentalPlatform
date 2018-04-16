d2d0531ab836214a13c4dfaf558995e6b3bb6aea
高洪滨 <gaohongbin_work@163.com>
2018-04-16 16:07:51 +0800

    项目架构优化
    添加web文件

--- src/main/java/com/rp/account/controller/AccountController.java
+++ src/main/java/com/rp/account/controller/AccountController.java
@@ -1,6 +1,10 @@
 package com.rp.account.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rp.account.service.AccountService;
 
 /**
  * 用户控制器
@@ -8,8 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 高洪滨
  *
  */
-@RestController
+@RestController()
+@RequestMapping("account")
 public class AccountController {
+
+	@Autowired
+	private AccountService service;
+
+	/**
+	 * 验证用户名是否存在
+	 * 
+	 * @param username
+	 */
+	@RequestMapping("verifyUsername")
+	public boolean verifyUsername(String username) {
+		return service.verifyUsername();
+	}
 
 	/**
 	 * 验证手机号是否存在
---
+++ src/main/java/com/rp/account/service/AccountService.java
@@ -0,0 +1,16 @@
+package com.rp.account.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+	public boolean verifyUsername() {
+		return false;
+	}
+
+	public boolean verifyPhone() {
+		return false;
+	}
+
+}
