9c565f10086bd64a8451d94f90cab54cb71ab787
高洪滨 <gaohongbin_work@163.com>
2018-04-17 20:29:39 +0800

    添加功能

---
+++ src/main/java/com/rp/system/controller/GotoController.java
@@ -0,0 +1,27 @@
+package com.rp.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GotoController {
+
+	@GetMapping("gotoTest")
+	public String gotoTest() {
+		return "html/test";
+	}
+	
+	@GetMapping("gotoHome")
+	public String gotoHome() {
+		return "html/index";
+	}
+
+	@GetMapping("gotoSignUp")
+	public String gotoSignUp() {
+		return "html/system/signup";
+	}
+
+	public String gotoSignIn() {
+		return "html/system/signin";
+	}
+}
--- src/main/resources/static/js/signin.js
+++
@@ -1,9 +0,0 @@
-function check(){
-	$.ajax({
-		url : "http://localhost:8888/rp/account/verifyUsername",
-		dataType : "text",
-		success : function(result) {
-			console.log(result);
-		}
-	});
-}
